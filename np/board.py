3706e1e0cd6f45b4104b7d8f90266b02a07db5ec
kensuke kubo <kensukekubo19@gmail.com>
2020-05-24 14:54:22 +0900

    集合内包表記にて行数節約

--- np/board.py
+++ np/board.py
@@ -18,27 +18,19 @@ class Board:
                 self.question.append([int(j) for j in list(i.replace('\n', ''))])
 
     def make_board(self):
-        self.board = []
-        for i in range(9):
-            self.board.append([])
-            for j in range(9):
-                if self.solving[i][j] == 0:
-                    self.board[i].append(
-                        {i for i in range(1, 10)} - self.row(i) - self.column(j) - self.block(j, i))
-                else:
-                    self.board[i].append({self.solving[i][j]})
+        self.board = [[{i for i in range(1, 10)} - self.check(i, j) if self.solving[i][j] == 0 else {self.solving[i][j]}
+                       for j in range(9)] for i in range(9)]
+
         self.before_solving = self.solving
-        self.solving = []
-        for i in range(9):
-            self.solving.append([])
-            for j in range(9):
-                if len(self.board[i][j]) == 1:
-                    self.solving[i].append(list(self.board[i][j])[0])
-                else:
-                    self.solving[i].append(0)
+        self.solving = [[list(self.board[i][j])[0] if len(self.board[i][j]) == 1 else 0
+                         for j in range(9)] for i in range(9)]
+
         if self.before_solving != self.solving:
             self.history.append(self.before_solving)
             self.make_board()
+
+    def check(self, i, j):
+        return self.row(i) | self.column(j) | self.block(j, i)
 
     def row(self, y):
         return {self.solving[y][_x] for _x in range(9) if self.solving[y][_x] != 0}
